29d58c8a7352d52e79f26b9ceb4ae463ebe558d5
Jorge Sanchez <jorjatorz@gmail.com>
2015-03-19 17:26:18 +0100

    Probando branch

--- src/tp/pr4/Main.java
+++ src/tp/pr4/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Main {
 	
-	//True if the user asked for help through an argument
+	//Set to true if user asked for help by argument
 	static private boolean helpDisplayed;
 	
 	public static void main(String[] args) {
